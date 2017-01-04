6a981f1e62b74f00b3e4038e984faff42a25c410
Filip Klimeš <filip@filipklimes.cz>
2017-01-04 15:47:23 +0100

    Fixed & extended tests

--- src/test/java/cz/cvut/fel/sep/klimefi1/semestral/ChangeRequestTests.java
+++ src/test/java/cz/cvut/fel/sep/klimefi1/semestral/ChangeRequestTests.java
@@ -28,27 +28,110 @@ public class ChangeRequestTests {
     public void showChangeRequestsList() throws Exception {
         mockMvc.perform(get("/change-requests"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("John Doe")))
-                .andExpect(content().string(containsString("Chuck Norris")));
+                .andExpect(content().string(containsString("SUSPEND")))
+                .andExpect(content().string(containsString("REMOVE")))
+                .andExpect(content().string(containsString("REFUND")));
     }
 
     @Test
     @WithMockUser
-    public void showChangeRequestForm() throws Exception {
-        mockMvc.perform(get("/change-requests/create"))
+    public void showAddChangeRequestForm() throws Exception {
+        mockMvc.perform(get("/change-requests/add"))
                 .andExpect(content().string(containsString("Create Change Request")));
     }
 
     @Test
     @WithMockUser
-    public void submitChangeRequestForm() throws Exception {
+    public void showEditChangeRequestForm() throws Exception {
+        mockMvc.perform(get("/change-requests/edit"))
+                .andExpect(content().string(containsString("Create Change Request")));
+    }
+
+    @Test
+    @WithMockUser
+    public void showRefundChangeRequestForm() throws Exception {
+        mockMvc.perform(get("/change-requests/refund"))
+                .andExpect(content().string(containsString("Create Change Request")));
+    }
+
+    @Test
+    @WithMockUser
+    public void showSuspendChangeRequestForm() throws Exception {
+        mockMvc.perform(get("/change-requests/suspend"))
+                .andExpect(content().string(containsString("Create Change Request")));
+    }
+
+    @Test
+    @WithMockUser
+    public void showRemoveChangeRequestForm() throws Exception {
+        mockMvc.perform(get("/change-requests/remove"))
+                .andExpect(content().string(containsString("Create Change Request")));
+    }
+
+    @Test
+    @WithMockUser
+    public void submitAddChangeRequestForm() throws Exception {
         mockMvc.perform(
-                post("/change-requests/create")
+                post("/change-requests/add")
                         .with(csrf())
                         .param("clientId", "12345")
-                        .param("type", "REMOVE")
-                        .param("note", "Because reasons..."))
+                        .param("firstName", "John")
+                        .param("surname", "Doe")
+                        .param("address", "Add")
+                        .param("phoneNum", "123456789")
+                        .param("birthNum", "123456789")
+                        .param("countryOfOrigin", "Czech Republic"))
                 .andExpect(status().is3xxRedirection())
-        .andExpect(redirectedUrl("/change-requests"));
+                .andExpect(redirectedUrl("/change-requests"));
+    }
+
+    @Test
+    @WithMockUser
+    public void submitEditChangeRequestForm() throws Exception {
+        mockMvc.perform(
+                post("/change-requests/edit")
+                        .with(csrf())
+                        .param("clientId", "12345")
+                        .param("firstName", "John")
+                        .param("surname", "Doe")
+                        .param("address", "Add")
+                        .param("phoneNum", "123456789")
+                        .param("birthNum", "123456789")
+                        .param("countryOfOrigin", "Czech Republic"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/change-requests"));
+    }
+
+    @Test
+    @WithMockUser
+    public void submitSuspendChangeRequestForm() throws Exception {
+        mockMvc.perform(
+                post("/change-requests/suspend")
+                        .with(csrf())
+                        .param("clientId", "12345"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/change-requests"));
+    }
+
+    @Test
+    @WithMockUser
+    public void submitRefundChangeRequestForm() throws Exception {
+        mockMvc.perform(
+                post("/change-requests/refund")
+                        .with(csrf())
+                        .param("clientId", "12345"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/change-requests"));
+    }
+
+    @Test
+    @WithMockUser
+    public void submitRemoveChangeRequestForm() throws Exception {
+        mockMvc.perform(
+                post("/change-requests/remove")
+                        .with(csrf())
+                        .param("clientId", "12345"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/change-requests"));
     }
 }
