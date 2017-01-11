4b0f168f1c9d28d43821b51c3787bf099608a39b
Filip Klimeš <filip@filipklimes.cz>
2017-01-11 13:34:29 +0100

    Added logout clear session

--- src/main/java/cz/cvut/fel/sep/klimefi1/semestral/WebSecurityConfig.java
+++ src/main/java/cz/cvut/fel/sep/klimefi1/semestral/WebSecurityConfig.java
@@ -22,6 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .logout()
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
                     .permitAll();
     }
 
